0fbb9b7cd978a163f761b42082dfc28b2fa9c8ef
RaeannTaiYuXuan <150000973+RaeannTaiYuXuan@users.noreply.github.com>
2025-02-04 00:24:38 +0800

    Update for main, command and adaptor classes (not done)

---
+++ SDP_Assignment/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_Assignment
+{
+    public class User
+    {
+        private string name;
+
+        public string Name { get; set; }
+
+        public User(string name) 
+        {
+            Name = name;
+        }
+    }
+}
