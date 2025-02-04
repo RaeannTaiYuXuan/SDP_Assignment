8d07512577f211c3f6cf9f6592875229a2352fed
Ng-Kai-Huat-Jason <S10262552@connect.np.edu.sg>
2025-02-04 23:41:02 +0800

    Factory Pattern for Document Types, Strategy for Format

---
+++ SDP_Assignment/Jason/TechnicalReportFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Factory Pattern
+
+namespace SDP_Assignment.Jason
+{
+    public class TechnicalReportFactory : IDocumentFactory
+    {
+        public Document CreateDocument(string title, string content, User owner)
+        {
+            return new Document(
+                title,
+                "TECHNICAL REPORT HEADER",
+                "Technical Report Footer",
+                owner
+            )
+            { Content = content };
+        }
+    }
+}
