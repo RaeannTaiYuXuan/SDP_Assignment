be0e8371ea9a8c8af6fd7071b77689517584f024
RaeannTaiYuXuan <150000973+RaeannTaiYuXuan@users.noreply.github.com>
2025-02-06 18:58:00 +0800

    update observer pattern

---
+++ SDP_Assignment/RAEANN/DocumentObserver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_Assignment.RAEANN
+{
+    public class DocumentObserver
+    {
+        private List<INotifiable> observers = new List<INotifiable>();
+
+        public void Attach(INotifiable observer)
+        {
+            if (!observers.Contains(observer))
+            {
+                observers.Add(observer);
+            }
+        }
+
+        public void Detach(INotifiable observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers(string message)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Notify(message);
+            }
+        }
+    }
+
+}
