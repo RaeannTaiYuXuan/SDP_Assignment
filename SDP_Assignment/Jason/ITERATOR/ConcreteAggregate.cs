56425748a7161a04bea7b9b47a0d07f650c60028
Ng-Kai-Huat-Jason <S10262552@connect.np.edu.sg>
2025-02-13 23:20:48 +0800

    Pushed Jason's Iterator and TestData Function

---
+++ SDP_Assignment/Jason/ITERATOR/ConcreteAggregate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_Assignment.Jason.ITERATOR
+{
+    public class DocumentAggregate : IAggregate
+    {
+        private List<Document> documents;
+
+        public DocumentAggregate(List<Document> documents)
+        {
+            this.documents = documents;
+        }
+
+        public int Count
+        {
+            get { return documents.Count; }
+        }
+
+        public Document GetDocument(int index)
+        {
+            return documents[index];
+        }
+
+        public IIterator CreateIterator()
+        {
+            return new DocumentIterator(this);
+        }
+    }
+
+}
